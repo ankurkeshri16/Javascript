2471efc544cacc9d2ffbfa80f78c6401a2c584be
Ankur Keshri <110325934+ankurkeshri16@users.noreply.github.com>
2024-02-26 13:23:24 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 12647
+let accountEmail = "ankur@google.com"
+var accountName = "Ankur"
+accountCity = "Patna"
+let accountState
+
+// accountId = 32455
+
+accountEmail = "keshri@google.com"
+accountName = "Keshri"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountName,accountCity,accountState])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
